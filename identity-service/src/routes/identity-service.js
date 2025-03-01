0de2c9b83855214a8d8ef3109ba3fd98605b9b95
ToseebNadaf <toseebnadaf@gmail.com>
2025-03-01 12:11:15 +0530

    Created controller & route for user registration

---
+++ identity-service/src/routes/identity-service.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { resgiterUser } = require("../controllers/identity-controller");
+
+const router = express.Router();
+
+router.post("/register", resgiterUser);
+
+module.exports = router;
